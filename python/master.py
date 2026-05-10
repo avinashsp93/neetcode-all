2ed71ad11336c600f964b840e7cf8d45b189565c
Avinash Sorab <avinashsp93@gmail.com>
2026-05-10 11:35:33 -0400

    is subsequence

--- python/master.py
+++ python/master.py
@@ -2,14 +2,17 @@ from typing import List
 # from arrays_and_hashing.p0001_two_sum import Solution
 # from arrays_and_hashing.p0217_contains_duplicate import Solution
 # from arrays_and_hashing.p0242_valid_anagram import Solution
-from arrays_and_hashing.p1299_replace_elements_with_greatest_element_on_right_side import Solution
+# from arrays_and_hashing.p1299_replace_elements_with_greatest_element_on_right_side import Solution
+from arrays_and_hashing.p392_is_subsequence import Solution
 
 def main() -> None:
     # Input
-    arr = [17,18,5,4,6,1]
+    # arr = [17,18,5,4,6,1]
+    s = "abc"
+    t = "ahbgdc"
 
-    result = Solution().replaceElements(arr)
-    print("containsDuplicate result:", result)
+    result = Solution().isSubsequence(s,t)
+    print("result:", result)
 
 if __name__ == "__main__":
     main()
