e90800fc0e898e0e191e058f60193880574737c4
Avinash Sorab <avinashsp93@gmail.com>
2026-05-15 22:18:13 -0400

    crawler log folder

--- python/master.py
+++ python/master.py
@@ -1,14 +1,11 @@
 from typing import List
-from l1_arrays_and_hashing.p0290_word_pattern import Solution
+from l2_stack.p1598_crawler_log_folder import Solution
 # from l1_arrays_and_hashing.p0205_isomorphic_strings import Solution
 
 def main() -> None:
     # Input
-    pattern = "aaaa"
-    s = "dog dog dog dog"
-    # pattern = "abba"
-    # s = "aaaa"
-    result = Solution().wordPattern(pattern, s)
+    logs = ["d1/","d2/","./","d3/","../","d31/"]
+    result = Solution().minOperations_stackApproach(logs)
     print("result:", result)
 
 if __name__ == "__main__":
