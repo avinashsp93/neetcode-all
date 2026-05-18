3017c6e3e64a32d8ff21a1bd860f354e488a5491
Avinash Sorab <avinashsp93@gmail.com>
2026-05-18 18:38:36 -0400

    license key formatting

--- python/master.py
+++ python/master.py
@@ -1,11 +1,11 @@
 from typing import List
-from l2_stack.p1598_crawler_log_folder import Solution
-# from l1_arrays_and_hashing.p0205_isomorphic_strings import Solution
+from l0_from_leetcode.p0482_license_key_formatting import Solution
 
 def main() -> None:
     # Input
-    logs = ["d1/","d2/","./","d3/","../","d31/"]
-    result = Solution().minOperations_stackApproach(logs)
+    s = "5F3Z-2e-9-w"
+    k = 2
+    result = Solution().licenseKeyFormatting(s, k)
     print("result:", result)
 
 if __name__ == "__main__":
