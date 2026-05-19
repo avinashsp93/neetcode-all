41c0aa6db5d857c0f6f5d44506d64b5a5359dc12
Avinash Sorab <avinashsp93@gmail.com>
2026-05-19 18:58:38 -0400

    2 problems

--- python/master.py
+++ python/master.py
@@ -1,11 +1,10 @@
 from typing import List
-from l0_from_leetcode.p0482_license_key_formatting import Solution
+from l0_from_leetcode.p0028_find_the_index_of_the_first_occurrence_in_a_string import Solution
 
 def main() -> None:
     # Input
-    s = "5F3Z-2e-9-w"
-    k = 2
-    result = Solution().licenseKeyFormatting(s, k)
+
+    result = Solution().strStr("a", "aaba")
     print("result:", result)
 
 if __name__ == "__main__":
