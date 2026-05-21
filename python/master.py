48b556882a1485b0855e4663269628d2c9cf47d4
Avinash Sorab <avinashsp93@gmail.com>
2026-05-20 22:52:06 -0400

    3 more problems

--- python/master.py
+++ python/master.py
@@ -1,10 +1,14 @@
 from typing import List
-from l0_from_leetcode.p0028_find_the_index_of_the_first_occurrence_in_a_string import Solution
+from l1_arrays_and_hashing.p0448_find_all_numbers_disappeared_in_an_array import Solution
 
 def main() -> None:
     # Input
+    nums = [4,3,2,7,8,2,3,1]
+    # nums = [1,1]
+    # nums = [1,4,4,4]
 
-    result = Solution().strStr("a", "aaba")
+
+    result = Solution().findDisappearedNumbers(nums)
     print("result:", result)
 
 if __name__ == "__main__":
