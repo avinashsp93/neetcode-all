911c19274edaad5415fcfa93af55cdd40ef466ca
Avinash Sorab <avinashsp93@gmail.com>
2026-06-08 21:53:20 -0400

    exactly one consecutive set bits pair

--- python/master.py
+++ python/master.py
@@ -1,10 +1,8 @@
 from typing import List
-from l2_stack.d1_easy.p3174_clear_digits import Solution
+from l3_linked_list.create_linked_list import ListNode
 
 def main() -> None:
-    s = "123"
-    result = Solution().clearDigits(s)
-    print(result)
+    return
 
 if __name__ == "__main__":
     main()
